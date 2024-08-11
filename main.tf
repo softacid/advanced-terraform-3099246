e7b60dfde24e3f633a612db9316ccdd50fa0d9ba
SoftAcid <admin@softacid.net>
2024-08-11 19:35:39 +0100

    updated space id gcloud

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "advancedterraform-432119" 
   region  = "us-central1"
   zone    = "us-central1-a"
 }
